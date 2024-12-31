b5b8c4833fe761d484858cd21d7da6925e0a2ff3
StefanTodoran <stefan.alex4@gmail.com>
2024-12-31 01:49:32 -0800

    Able to fetch all official levels from the server

---
+++ dev/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
+import { collection, DocumentData, getDocs, getFirestore, QuerySnapshot } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASxDTiQ2ahy8fhVtllCnQ4Lbh2bhqsr9A",
+    authDomain: "crates-n-craters.firebaseapp.com",
+    projectId: "crates-n-craters",
+    storageBucket: "crates-n-craters.appspot.com",
+    messagingSenderId: "967442204309",
+    appId: "1:967442204309:web:062c693d6beac71d4cbd9c"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = initializeAuth(app);
+
+export async function getAllEntries(collectionName: string) {
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    return parseQuerySnapshot(querySnapshot);
+}
+
+function parseQuerySnapshot(snapshot: QuerySnapshot<DocumentData, DocumentData>) {
+    const docs: any[] = [];
+    snapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        const next = doc.data();
+        next.id = doc.id;
+        next.uuid = doc.id;
+        docs.push(next);
+    });
+
+    return docs;
+}
---
+++ dev/pull.ts
@@ -0,0 +1,42 @@
+import fse from "fs-extra";
+import { getAllEntries } from "./firebase";
+import { log, LogStatus } from "./logging";
+
+async function getOrderedRawLevels() {
+    const rawLevels = await getAllEntries("officialLevels");
+    rawLevels.sort((levelA, levelB) => levelA.order - levelB.order);
+    return rawLevels;
+} 
+
+async function main() {
+    const rawLevels = await getOrderedRawLevels();
+    log(`Successfully received ${rawLevels.length} levels from server.`, LogStatus.GOOD);
+    
+    fse.removeSync("./levels");
+    fse.mkdirSync("./levels");
+
+    // Write each level to a file in the levels folder, as a json.
+    let successCount = 0;
+    rawLevels.forEach(level => {
+        const levelFile = `./levels/${level.id}.json`;
+        fse.writeFileSync(levelFile, JSON.stringify(level, undefined, 4));
+        if (!fse.existsSync(levelFile)) {
+            log(`Write to ${levelFile} failed! Levels have not been saved.\n`, LogStatus.ERROR);
+        } else {
+            successCount++;
+        }
+    });
+    
+    if (successCount === rawLevels.length) {
+        log(`Successfully wrote all ${successCount} raw levels to levels folder!\n`, LogStatus.GOOD);
+        // @ts-ignore This method exists... not sure why TypeScript is bugging out.
+        process.exit(0);
+    } else {
+        log(`Failed to write ${rawLevels.length - successCount} levels to levels folder!\n`, LogStatus.ERROR);
+        // @ts-ignore
+        process.exit(1);
+    }
+}
+
+log("\nFetching official levels from server...");
+main();
